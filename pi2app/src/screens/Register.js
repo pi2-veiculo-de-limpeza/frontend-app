968dd4e909a8fc589f15939ebf00119b82891d07
Hugo Alves <hugo.asb96@gmail.com>
2018-10-21 18:17:56 -0300

    Creating field validations.

--- pi2app/src/screens/Register.js
+++ pi2app/src/screens/Register.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { ScrollView, 
+import { 
+  ScrollView, 
   ImageBackground,
   StyleSheet,
-  KeyboardAvoidingView } from "react-native";
+  KeyboardAvoidingView,
+  Alert } from "react-native";
 import { 
   Card, 
   Button, 
@@ -28,7 +30,11 @@ class Register extends React.Component{
       name: '',
       email: '',
       password: '',
-      passwordAgain: ''
+      passwordAgain: '',
+      nameIsValid: false,
+      emailIsValid: false,
+      passwordIsValid: false,
+      confirmPassword: false
     };
   }
 
@@ -47,6 +53,79 @@ class Register extends React.Component{
     },
   };
 
+  showAlert(title, body){
+    Alert.alert(
+      title,
+      body,
+      [
+        {text: 'Ok', onPress: () => console.log('Ok pressed')},
+      ],
+      { cancelable: false }
+    )
+  }
+
+  submitRegister = () => {
+    const titleAlert = 'Campo(s) Inválido(s)';
+    const bodyAlert = 'Não foi possível realizar o cadastro porque existem entradas inválidas!';
+    if(this.state.nameIsValid && 
+       this.state.emailIsValid &&
+       this.state.passwordIsValid &&
+       this.state.confirmPassword){
+    // TODO: create POST method
+    // TODO: change parameter to receive token from the POST request
+      onSignIn(this.state.name)
+      .then(() => this.props.navigation.navigate("MainScreen"));
+    } else {
+      this.showAlert(titleAlert, bodyAlert);
+    }
+  };
+
+  // Methods to handle validity of inputs
+  validateName = (text) => {
+    const validName = text.replace(/[^A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]/g, '');
+
+    if(validName.length > 3){
+      this.setState({ nameIsValid: true });
+    } else {
+      this.setState({ nameIsValid: false })
+    }
+
+    this.setState({ name: validName });
+  };
+
+  validateEmail = (text) => {
+    const emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    const validEmail = emailRegex.test(text);
+    
+    if(validEmail){
+      this.setState({ emailIsValid: true });
+    } else {
+      this.setState({ emailIsValid: false });
+    }
+
+    this.setState({ email: text });
+  };
+
+  validatePassword = (text) => {
+    if(text.length > 5 && text.length < 19){
+      this.setState({ passwordIsValid: true });
+    } else {
+      this.setState({ passwordIsValid: false })
+    }
+
+    this.setState({ password: text });
+  };
+
+  comparePassword = (text) => {
+    if(this.state.password === text){
+      this.setState({ confirmPassword: true })
+    } else {
+      this.setState({ confirmPassword: false })
+    }
+
+    this.setState({ passwordAgain: text });
+  };
+
   render(){
     return(
       <ImageBackground style={styles.initialBackgroundImage} source={initialBackgroundImage}>
@@ -56,36 +135,48 @@ class Register extends React.Component{
             <FormLabel>Nome</FormLabel>
             <FormInput 
               placeholder="Digite seu nome"
-              onChangeText={(text) => this.setState({name: text})} 
+              onChangeText={(text) => this.validateName(text)}
+              value={this.state.name}
             />
+            {this.state.nameIsValid == false && this.state.name.length != 0 &&
+              <FormValidationMessage>Nome inválido!</FormValidationMessage>
+            }
 
-            <FormLabel>E-mail</FormLabel>
-            <FormInput 
+            <FormLabel>Email</FormLabel>
+            <FormInput
+              keyboardType='email-address'
               placeholder="Digite seu e-mail"
-              onChangeText={(text) => this.setState({email: text})}
+              onChangeText={(text) => this.validateEmail(text)}
             />
+            {this.state.emailIsValid == false && this.state.email.length != 0 &&
+              <FormValidationMessage>Email inválido!</FormValidationMessage>
+            }
 
             <FormLabel>Senha</FormLabel>
             <FormInput 
               secureTextEntry 
               placeholder="Digite sua senha"
-              onChangeText={(text) => this.setState({password: text})}
+              onChangeText={(text) => this.validatePassword(text)}
             />
+            {this.state.passwordIsValid == false && this.state.password.length != 0 &&
+              <FormValidationMessage>A senha deve ter entre 6 e 18 caracteres!</FormValidationMessage>
+            }
 
             <FormLabel>Confirmação da Senha</FormLabel>
             <FormInput 
               secureTextEntry
               placeholder="Confirme sua senha"
-              onChangeText={(text) => this.setState({passwordAgain: text})} 
+              onChangeText={(text) => this.comparePassword(text)} 
             />
+            {this.state.confirmPassword == false && this.state.passwordAgain.length != 0 &&
+              <FormValidationMessage>As senhas digitadas são diferentes!</FormValidationMessage>
+            }
 
             <Button
               buttonStyle={{ marginTop: 20 }}
               backgroundColor="#03A9F4"
               title="Entrar"
-              onPress={() => {
-                onSignIn(this.state.name).then(() => this.props.navigation.navigate("MainScreen"));
-              }}
+              onPress={() => {this.submitRegister()}}
             />
           </Card>
           </ScrollView>
