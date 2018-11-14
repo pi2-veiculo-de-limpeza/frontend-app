7691d5b4966a53286fb8de70cf612c907e90ee4d
Hugo Alves <hugo.asb96@gmail.com>
2018-11-14 01:07:19 -0200

    Displaying Map and drawing polygon.

--- pi2app/src/screens/VehicleDetail.js
+++ pi2app/src/screens/VehicleDetail.js
@@ -2,47 +2,212 @@ import React from 'react';
 import { 
   Text,
   View,
-  TouchableHighlight,
-  ScrollView,
-  RefreshControl } from 'react-native';
-import { Card, ListItem, Button, Icon, Badge } from 'react-native-elements';
-import { onSignOut, getUserToken } from "../AuthMethods";
-import styles from '../styles/GeneralStyles';
-import { INITIAL_BACKGROUND_IMG } from '../constants/GeneralConstants';
+  Dimensions,
+  TouchableOpacity,
+  StyleSheet } from 'react-native';
+import MapView, { MAP_TYPES, Polygon, ProviderPropType } from 'react-native-maps';
+
+const { width, height } = Dimensions.get('window');
+
+const ASPECT_RATIO = width / height;
+const LATITUDE = -15.867900;
+const LONGITUDE = -48.081260;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+let id = 0;
+
+// TODO: Ver problema do fillColor, buscar meio de abrir mapa com zoom
+// e pegar coordenadas atuais do usuário
 
 class VehicleDetail extends React.Component {
   state = {
     token: '',
-    vehicle: {}
+    vehicle: {},
+    region: {
+      latitude: LATITUDE,
+      longitude: LONGITUDE,
+      latitudeDelta: LATITUDE_DELTA,
+      longitudeDelta: LONGITUDE_DELTA,
+    },
+    polygons: [],
+    editing: null,
+    creatingHole: false,
   }
 
-  // Navigation header
-  static navigationOptions = {
-    title: 'Vehicle Detail',
-    headerStyle: {
-      backgroundColor: '#53A9F6',
-      elevation: 0,
-      borderBottomWidth: 0,
-    },
-    headerTintColor: '#fff',
-    headerTitleStyle: {
-      alignSelf:'center',
-      fontWeight: 'bold',
-      fontSize: 35,
-    },
-  };
+  finish() {
+    const { polygons, editing } = this.state;
+    this.setState({
+      polygons: [...polygons, editing],
+      editing: null,
+      creatingHole: false,
+    });
+  }
+
+  createHole() {
+    const { editing, creatingHole } = this.state;
+    if (!creatingHole) {
+      this.setState({
+        creatingHole: true,
+        editing: {
+          ...editing,
+          holes: [
+            ...editing.holes,
+            [],
+          ],
+        },
+      });
+    } else {
+      const holes = [...editing.holes];
+      if (holes[holes.length - 1].length === 0) {
+        holes.pop();
+        this.setState({
+          editing: {
+            ...editing,
+            holes,
+          },
+        });
+      }
+      this.setState({ creatingHole: false });
+    }
+  }
+
+  onPress(e) {
+    const { editing, creatingHole } = this.state;
+    if (!editing) {
+      this.setState({
+        editing: {
+          id: id++,
+          coordinates: [e.nativeEvent.coordinate],
+          holes: [],
+        },
+      });
+    } else if (!creatingHole) {
+      this.setState({
+        editing: {
+          ...editing,
+          coordinates: [
+            ...editing.coordinates,
+            e.nativeEvent.coordinate,
+          ],
+        },
+      });
+    } else {
+      const holes = [...editing.holes];
+      holes[holes.length - 1] = [
+        ...holes[holes.length - 1],
+        e.nativeEvent.coordinate,
+      ];
+      this.setState({
+        editing: {
+          ...editing,
+          id: id++, // keep incrementing id to trigger display refresh
+          coordinates: [
+            ...editing.coordinates,
+          ],
+          holes,
+        },
+      });
+    }
+  }
 
   render() {
+    const mapOptions = {
+      scrollEnabled: true,
+    };
+
+    if (this.state.editing) {
+      mapOptions.scrollEnabled = false;
+      mapOptions.onPanDrag = e => this.onPress(e);
+    }
+
     return (
-      <ScrollView contentContainerStyle={styles.vehicleScrollView}>
-        <View>
-            <Text>
-                Vehicle Detail
-            </Text>
+      <View style={styles.container}>
+        <MapView
+          provider={this.props.provider}
+          style={styles.map}
+          mapType={MAP_TYPES.HYBRID}
+          initialRegion={this.state.region}
+          onPress={e => this.onPress(e)}
+          {...mapOptions}
+        >
+          {this.state.polygons.map(polygon => (
+            <Polygon
+              key={polygon.id}
+              coordinates={polygon.coordinates}
+              holes={polygon.holes}
+              fillColor="rgba(255, 0, 0, 0.3)"
+              strokeColor="#eeff00"
+              strokeWidth={2}
+            />
+          ))}
+          {this.state.editing && (
+            <Polygon
+              key={this.state.editing.id}
+              coordinates={this.state.editing.coordinates}
+              holes={this.state.editing.holes}
+              strokeColor="#000"
+              fillColor="rgba(255,0,0,0.5)"
+              strokeWidth={1}
+            />
+          )}
+        </MapView>
+        <View style={styles.buttonContainer}>
+          {this.state.editing && (
+            <TouchableOpacity
+              onPress={() => this.createHole()}
+              style={[styles.bubble, styles.button]}
+            >
+              <Text>{this.state.creatingHole ? 'Finish Hole' : 'Create Hole'}</Text>
+            </TouchableOpacity>
+          )}
+          {this.state.editing && (
+            <TouchableOpacity
+              onPress={() => this.finish()}
+              style={[styles.bubble, styles.button]}
+            >
+              <Text>Finish</Text>
+            </TouchableOpacity>
+          )}
         </View>
-      </ScrollView>
+      </View>
     );
   }
 }
+
+VehicleDetail.propTypes = {
+  provider: ProviderPropType,
+};
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  bubble: {
+    backgroundColor: 'rgba(255,255,255,0.7)',
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    borderRadius: 20,
+  },
+  latlng: {
+    width: 200,
+    alignItems: 'stretch',
+  },
+  button: {
+    width: 80,
+    paddingHorizontal: 12,
+    alignItems: 'center',
+    marginHorizontal: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginVertical: 20,
+    backgroundColor: 'transparent',
+  },
+});
 
 export default VehicleDetail;
