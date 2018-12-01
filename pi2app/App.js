9c03bf5cbbb864bd58136dfd66313fe15b385063
Lucas Costa <lucascostaa73@gmail.com>
2018-12-01 12:22:56 -0200

    Adding travis

--- pi2app/App.js
+++ pi2app/App.js
@@ -3,9 +3,9 @@ import { RootNavigator } from './src/Routes';
 import { isSignedIn } from "./src/AuthMethods";
 
 if (process.env.BACKEND == undefined || process.env.BACKEND == ''){
-  process.env.BACKEND='http://172.29.110.203:3000'
+  process.env.BACKEND=`http://${REACT_NATIVE_PACKAGER_HOSTNAME}:3000`
 }
-// process.env.BACKEND='http://18.216.157.143'
+process.env.BACKEND='http://18.216.157.143'
 
 class App extends React.Component {
   state = {
